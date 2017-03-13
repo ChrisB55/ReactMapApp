da4ca195ec995d802b83393ced66e97a99e2ac3f
Christopher W. Brown <christopherwilliambrown@gmail.com>
2017-03-13 16:42:41 -0400

    Add nav components
    Adjustments to map component

--- app/app.jsx
+++ app/app.jsx
@@ -2,11 +2,17 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Main');
+var About = require('About');
+var Example = require('Example');
 var GoogleMap = require('GoogleMap');
+
+
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
+    <Route path="about" component={About}/>
+      <Route path="example" component={Example}/>
       <IndexRoute component={GoogleMap}/>
     </Route>
   </Router>,
